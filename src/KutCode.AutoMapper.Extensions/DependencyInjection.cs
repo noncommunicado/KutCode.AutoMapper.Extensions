a44e4aa97707f1a2b7e8bc8af80b18fd9a5f8b5e
hamaronooo <hamaronooo1989@gmail.com>
2025-03-17 13:04:25 +0300

    v1.2.0 - DI updated

--- src/KutCode.AutoMapper.Extensions/DependencyInjection.cs
+++ src/KutCode.AutoMapper.Extensions/DependencyInjection.cs
@@ -9,20 +9,33 @@ namespace AutoMapper;
 public static class DependencyInjection
 {
 	private static Type _iMapWith = typeof(IMapWith<>);
-	
+
 	/// <summary>
 	/// Add all profiles from Application Domain assemblies
 	/// </summary>
 	/// <param name="services">A collection of service descriptors</param>
-	public static IServiceCollection AddAllMappings(this IServiceCollection services)
-		=> AddMappings(services, AppDomain.CurrentDomain.GetAssemblies());
-	
+	/// <param name="configAction">Configuration action to specify configuration of AutoMapper</param>
+	public static IServiceCollection AddAllMappings(this IServiceCollection services, Action<IMapperConfigurationExpression>? configAction = null)
+	{
+		return AddMappings(services, configAction, AppDomain.CurrentDomain.GetAssemblies());
+	}
+
 	/// <summary>
 	/// Add sprecified profiles from assemblies
 	/// </summary>
 	/// <param name="services">A collection of service descriptors</param>
 	/// <param name="assemblies">Assemblies to scan object inhirited from <see cref="IMapTo{TDestination}"/></param>
 	public static IServiceCollection AddMappings(this IServiceCollection services, params Assembly[] assemblies)
+	{
+		return services.AddMappings(configAction: null, assemblies);
+	}
+
+	/// <summary>
+	/// Add sprecified profiles from assemblies
+	/// </summary>
+	/// <param name="services">A collection of service descriptors</param>
+	/// <param name="assemblies">Assemblies to scan object inhirited from <see cref="IMapTo{TDestination}"/></param>
+	public static IServiceCollection AddMappings(this IServiceCollection services, Action<IMapperConfigurationExpression>? configAction = null, params Assembly[] assemblies)
 	{
 		services.AddAutoMapper(cfg =>{
 			foreach (var assembly in assemblies) {
@@ -35,8 +48,20 @@ public static class DependencyInjection
 				if (types.Length == 0) continue;
 				cfg.AddProfile(new AssemblyMappingProfile(types));
 			}
+			if (configAction is not null)
+				configAction.Invoke(cfg);
 		});
 		
+		return services;
+	}
+
+	/// <summary>
+	/// Add AutoMapper with explicit configuration in configAction
+	/// </summary>
+	/// <param name="configAction">Configuration action</param>
+	public static IServiceCollection AddMappings(this IServiceCollection services, Action<IMapperConfigurationExpression> configAction)
+	{
+		services.AddAutoMapper(configAction);
 		return services;
 	}
 }
